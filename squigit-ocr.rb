ecc9faca93e08d3961357c4f8c893abfdf293516
a7mddra <a7mddra@gmail.com>
2026-04-18 15:34:51 +0200

    chore(homebrew): update squigit-ocr formula homepage

--- squigit-ocr.rb
+++ squigit-ocr.rb
@@ -1,6 +1,6 @@
 class SquigitOcr < Formula
   desc "Standalone purely headless CLI OCR engine for Squigit"
-  homepage "https://github.com/a7mddra/squigit"
+  homepage "https://github.com/squigit-org/squigit"
   url "https://github.com/squigit-org/squigit/releases/download/ocr-v0.1.0/squigit-ocr-mac-aarch64.tar.gz"
   sha256 "5475dbd4f113b554c383f59da62d6ac9a309fd2d914c91e2dc75bbe96ec4b9ff"
   version "0.1.0"
